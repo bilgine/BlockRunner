aaa0e5fae79f567d741d687d837796211ca03679
Emir Bilgin <emir.bilgin@fugo.com.tr>
2023-05-14 14:43:25 +0300

    Sound System is added

---
+++ Assets/Scripts/SoundSystem/ISound.cs
@@ -0,0 +1,10 @@
+namespace SoundSystem
+{
+    public interface ISound
+    {
+        public void PlaySound();
+    
+        public void ResetSound();
+        public void PitchSound();
+    }
+}
---
+++ Assets/Scripts/SoundSystem/SoundManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace SoundSystem
+{
+    public class SoundManager : MonoBehaviour, ISound
+    {
+        [SerializeField] private AudioSource audioSource;
+        public void PlaySound()
+        {
+            audioSource.volume = 1f;
+            audioSource.Play();
+        }
+
+        public void ResetSound()
+        {
+            audioSource.pitch = 1f;
+        }
+    
+        public void PitchSound()
+        {
+            audioSource.pitch += 0.05f;
+        }
+    }
+}
